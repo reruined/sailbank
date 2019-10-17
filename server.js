efa6b66711b44f8766f2a5d35790095c55725277
Alexander Szmidt <alexander.szmidt@gmail.com>
2019-10-17 19:28:17 +0200

    Add list

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const fetch = require('node-fetch')
 const papa = require('papaparse')
+const xml2js = require('xml2js')
 
 const port = 1337
 const bankDataSrc = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0BFdfpHUGmyGD6wyw8fcgADKA-CQXIosKU-JA6k4o5mjp-oa4UDRG5cxJOyeiOpUB74tSPjCiEHa1/pub?output=csv';
@@ -11,10 +12,32 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   fetch(bankDataSrc)
-    .then(data => data.text())
-    .then(body => {
-      const bankData = papa.parse(body, { header: true, dynamicTyping: true })
-      res.render('index', bankData.data)
+    .then(res => res.text())
+    .then(csv => {
+      const parsedCsv = papa.parse(csv, { header: true, dynamicTyping: true })
+      console.log('parsedCsv:')
+      console.log(parsedCsv)
+
+      return Promise.all(parsedCsv.data.map(item => {
+        return new Promise((resolve, reject) => {
+          const itemName = item.Item.toLowerCase()
+          const wowheadUrl = `https://www.classic.wowhead.com/item=${escape(itemName)}&xml`
+
+          fetch(wowheadUrl)
+            .then(res => res.text())
+            .then(xml => xml2js.parseStringPromise(xml))
+            .then(parsedXml => {
+              console.log()
+              const id = parsedXml.wowhead.item[0]['$'].id
+              const icon = parsedXml.wowhead.item[0].icon[0]._
+              resolve(Object.assign({}, item, {id, icon}))
+            })
+        })
+      }))
+    })
+    .then(items => {
+      console.log(items)
+      res.render('index', { contents: items })
     })
 })
 
